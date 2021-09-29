0a22d4963e9a7700d8b0db776538c3797710d413
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-09-29 01:58:06 -0700

    implemented redux functions testing to make sure it works

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { idbPromise } from "../../utils/helpers"
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
-import { useStoreContext } from "../../utils/GlobalState";
+// import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch, useSelector } from 'react-redux';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 import { QUERY_CHECKOUT } from '../../utils/queries';
@@ -10,7 +11,9 @@ import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
   
   const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
